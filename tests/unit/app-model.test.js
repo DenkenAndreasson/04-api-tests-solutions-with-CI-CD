798a9a9c319880beca3b7801999e9b4e9ea6b6a2
Dennis Andreasson <dennisandreasson@outlook.com>
2026-03-23 11:31:42 +0100

    init

--- tests/unit/app-model.test.js
+++ tests/unit/app-model.test.js
@@ -9,7 +9,7 @@ describe("app-model", () => {
   });
 
   test("posts starts as empty array", () => {
-    expect(model.posts).toEqual([]);
+    expect(model.posts).toEqual(["False"]);
   });
 
   test("reset clears all state", () => {
